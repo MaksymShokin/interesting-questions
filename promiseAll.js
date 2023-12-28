ac8b2d5c63f9be8305a2d2fc41fdef867982e25e
Maksym Shokin <problaze7@gmail.com>
2023-12-28 14:54:44 +0200

    improve promise all

--- promiseAll.js
+++ promiseAll.js
@@ -1,37 +1,33 @@
-function customPromiseAll(promises) {
-  return new Promise((resolve, reject) => {
-    const results = [];
-    let completedCount = 0;
+const promiseAll = promises => {
+  let resolved = 0;
 
-    if (promises.length === 0) {
-      resolve(results);
-      return;
-    }
+  return new Promise((resolve, reject) => {
+    const result = [];
 
     promises.forEach((promise, index) => {
-      Promise.resolve(promise)
-        .then(result => {
-          results[index] = result;
-          completedCount++;
+      promise
+        .then(value => {
+          result[index] = value;
+          resolved += 1;
 
-          if (completedCount === promises.length) {
-            resolve(results);
+          if (resolved === promises.length) {
+            resolve(result);
           }
         })
-        .catch(error => {
-          reject(error);
-        });
+        .catch(reject);
     });
   });
-}
+};
 
 // Example usage:
 const promise1 = Promise.resolve('Data from Promise 1');
 const promise2 = new Promise(resolve =>
   setTimeout(() => resolve('Data from Promise 2'), 1000)
 );
-const promise3 = Promise.reject(new Error('Error from Promise 3'));
+const promise3 = new Promise(resolve =>
+  setTimeout(() => resolve('Return from Promise 3'), 2000)
+);
 
-customPromiseAll([promise1, promise2, promise3])
+promiseAll([promise1, promise2, promise3])
   .then(results => console.log('All promises resolved:', results))
   .catch(error => console.error('Error in one or more promises:', error));
